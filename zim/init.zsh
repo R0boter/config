219a0cc93cbe2cd40f3f6bd9fe78b111a9c37d9f
Leon <leonsout@outlook.com>
2022-03-16 14:17:45 +0800

    更改zsh init文件中的用户名

--- zim/init.zsh
+++ zim/init.zsh
@@ -1,14 +1,14 @@
-zimfw() { source /root/.config/zim/zimfw.zsh "${@}" }
-fpath=(/root/.config/zim/modules/git/functions /root/.config/zim/modules/utility/functions /root/.config/zim/modules/git-info/functions ${fpath})
+zimfw() { source ~/.config/zim/zimfw.zsh "${@}" }
+fpath=(~/.config/zim/modules/git/functions ~/.config/zim/modules/utility/functions ~/.config/zim/modules/git-info/functions ${fpath})
 autoload -Uz git-alias-lookup git-branch-current git-branch-delete-interactive git-branch-remote-tracking git-dir git-ignore-add git-root git-stash-clear-interactive git-stash-recover git-submodule-move git-submodule-remove mkcd mkpw coalesce git-action git-info
-source /root/.config/zim/modules/environment/init.zsh
-source /root/.config/zim/modules/git/init.zsh
-source /root/.config/zim/modules/input/init.zsh
-source /root/.config/zim/modules/termtitle/init.zsh
-source /root/.config/zim/modules/utility/init.zsh
-source /root/.config/zim/modules/sorin/sorin.zsh-theme
-source /root/.config/zim/modules/zsh-completions/zsh-completions.plugin.zsh
-source /root/.config/zim/modules/completion/init.zsh
-source /root/.config/zim/modules/zsh-autosuggestions/zsh-autosuggestions.zsh
-source /root/.config/zim/modules/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-source /root/.config/zim/modules/zsh-history-substring-search/zsh-history-substring-search.zsh
+source ~/.config/zim/modules/environment/init.zsh
+source ~/.config/zim/modules/git/init.zsh
+source ~/.config/zim/modules/input/init.zsh
+source ~/.config/zim/modules/termtitle/init.zsh
+source ~/.config/zim/modules/utility/init.zsh
+source ~/.config/zim/modules/sorin/sorin.zsh-theme
+source ~/.config/zim/modules/zsh-completions/zsh-completions.plugin.zsh
+source ~/.config/zim/modules/completion/init.zsh
+source ~/.config/zim/modules/zsh-autosuggestions/zsh-autosuggestions.zsh
+source ~/.config/zim/modules/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source ~/.config/zim/modules/zsh-history-substring-search/zsh-history-substring-search.zsh
